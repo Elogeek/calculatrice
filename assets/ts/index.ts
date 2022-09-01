c7112f33958290154cd5c0137cfc5d3ff410761e
elogeek <elodiechristin@gmail.com>
2022-09-01 15:39:08 +0200

    finish project

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,82 +1,90 @@
+// Search btns of the calculator
 const buttons = document.querySelectorAll(".button") as NodeListOf<Element>;
 const output = document.querySelector(".output") as HTMLElement;
 
-// Search btn
-const reset = document.getElementById("clear") as HTMLButtonElement;
-const deleteBtn = document.getElementById("delete") as HTMLButtonElement;
-const equal = document.getElementById("equals") as HTMLButtonElement;
-
-let i: number = 0;
-
+// tests btns
 const arrayButtons: number = buttons.length;
 console.log(arrayButtons); // it's correct
 
-for(i = 0; i <arrayButtons; i++) {
-    // @ts-ignore
-    buttons[i].addEventListener("click", compute);
-    //console.log(buttons[i]); it's correct
-}
+let number1: string = "";
+let number2: string = "";
+let operator: string;
 
-type Operator = "+" | "-" | "*"| "/" ;
-
-/**
- * Action touch buttons
- */
-function actionBtn(): void {
-    const allButton: NodeListOf<Element> = document.querySelectorAll(".button");
-
-    allButton.forEach(function (e:Element):void {
-
-        e.addEventListener("click", function ():void {
-            console.log(e.innerHTML);
-            output.innerHTML = e.innerHTML;
-
-            // Special calculator button clicks
-            if(e.innerHTML === "clear") {
-                reset.addEventListener("click", e => output.innerText = " ")
-            }
-            if(e.innerHTML === "delete") {
-                deleteBtn.addEventListener("click", ev => {
-                    output.innerText = output.innerHTML.slice(0, output.innerHTML.length - 1)
-                });
-            }
-            // @ts-ignore
-            if(e.innerHTML === "=") {
-                equal.addEventListener("click", ev => {
-                        output.innerHTML = eval(output.innerHTML)
+for (let i: number = 0; i < buttons.length; i++) {
+   buttons[i].addEventListener("click", ()=>{
+        switch (buttons[i].innerHTML) {
+            case "+":
+                operator = "+";
+                output.innerHTML += " " + operator
+                break;
+            case "-":
+                operator = "-";
+                output.innerHTML += " " + operator
+                break;
+            case "*":
+                operator = "*";
+                output.innerHTML += " " + operator
+                break;
+            case "/":
+                operator = "/";
+               output.innerHTML += " " + operator
+                break;
+            case "Clear":
+                operator = "";
+                number1 = "";
+                number2 = "";
+                output.innerHTML = "";
+                break;
+            case "Delete":
+                operator = "";
+                number1 = "";
+                number2 = "";
+                output.innerHTML = "";
+                break;
+            case ".":
+                if (!operator){
+                    number1 += ".";
+                    output.innerHTML = number1;
                 }
-            )}
-        })
-
+                else {
+                    number2 += ".";
+                    output.innerHTML = number1 + " " + operator + " " + number2
+                }
+                break;
+            case "=":
+                let nbr1: number = parseFloat(number1);
+                let nbr2: number = parseFloat(number2);
+                if (operator === "+"){
+                    output.innerHTML = (nbr1 + nbr2).toString()
+                    number1 = "";
+                    number2 = "";
+                }
+                else if (operator === "-"){
+                    output.innerHTML = (nbr1 - nbr2).toString()
+                    number1 = "";
+                    number2 = "";
+                }
+                else if (operator === "*"){
+                    output.innerHTML = (nbr1 * nbr2).toString()
+                    number1 = "";
+                    number2 = "";
+                }
+                else if (operator === "/"){
+                    output.innerHTML = (nbr1 / nbr2).toString()
+                    number1 = "";
+                    number2 = "";
+                }
+                break;
+            default:
+                if (!operator){
+                    number1 += buttons[i].innerHTML;
+                    output.innerHTML = number1;
+                }
+                else {
+                    number2 += buttons[i].innerHTML;
+                    output.innerHTML = number1 + " " + operator + " " + number2
+                }
+                break;
+        }
     })
-
 }
-
-/**
- * Return compute (result of the calc)
- * @param numberA
- * @param operator
- * @param numberB
- */
-function compute(numberA: number, operator: Operator, numberB: number) : number | undefined {
-
-    switch (operator) {
-
-        case "+":
-            output.innerHTML = String(numberA + numberB);
-            return numberA + numberB;
-        case "-":
-            output.innerHTML = String(numberA - numberB);
-            return numberA - numberB;
-        case "*":
-            output.innerHTML = String(numberA * numberB);
-            return numberA * numberB;
-        case "/":
-            output.innerHTML = String( numberA / numberB);
-            return numberA / numberB;
-        default:
-            return 0;
-    }
-}
-
-actionBtn();
